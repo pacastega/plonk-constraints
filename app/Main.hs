8e7138b2f1b5596375bd447f49989a29ae100531
Pablo <p.cas.gar@gmail.com>
2024-02-20 10:21:39 +0100

    Implement simple main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
+{-# LANGUAGE DataKinds #-}
 module Main (main) where
 
-import Lib
+import Interpolation (interpolate)
+import Data.FiniteField.PrimeField
+import qualified Data.Vector as V (fromList, zip)
+import Data.Poly
 
 main :: IO ()
-main = someFunc
+main = do
+
+  let xs = V.fromList [1,2,3]
+  let ys = V.fromList [7,-2,3]
+
+  let q = interpolate 3 xs ys :: VPoly (PrimeField 101)
+  print q
+
+  putStrLn "Actual - Desired"
+  mapM_ (print . (\(x, y) -> (eval q x, y))) (V.zip xs ys)
+
+
+  -- let x = recip 7 :: PrimeField 101
+  -- print x
