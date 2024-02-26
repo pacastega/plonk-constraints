2a83fec301a304058b98383d9f5e039aa5b1dd5e
Pablo <p.cas.gar@gmail.com>
2024-02-26 13:33:24 +0100

    Simple test for arithmetic gates

--- app/Main.hs
+++ app/Main.hs
@@ -2,24 +2,27 @@
 {-# LANGUAGE DataKinds #-}
 module Main (main) where
 
+import qualified Data.Vector as V (fromList, zip, iterateN)
+
 import Interpolation (interpolateRoots)
 import Data.FiniteField.PrimeField
 import PrimitiveRoot
-import qualified Data.Vector as V (fromList, zip, iterateN)
 import Data.Poly
+
+-- import ArithmeticGates
+-- import Constraints
 
 main :: IO ()
 main = do
 
   let xs = V.iterateN 4 (* (primitiveRoot ^ (16 `div` 4))) 1
   let ys = V.fromList [7,-2,3,10]
-
   let q = interpolateRoots 17 4 ys :: VPoly (PrimeField 17)
   print q
-
   putStrLn "Actual - Desired"
   mapM_ (print . (\(x, y) -> (eval q x, y))) (V.zip xs ys)
 
-
-  -- let x = recip 7 :: PrimeField 101
-  -- print x
+  -- print $ satisfies 1 3 (V.fromList [2,3,5]) addGate -- True
+  -- print $ satisfies 1 3 (V.fromList [2,3,6]) addGate -- False
+  -- print $ satisfies 1 3 (V.fromList [5,3,10]) mulGate -- False
+  -- print $ satisfies 1 3 (V.fromList [5,3,-2]) mulGate -- True
