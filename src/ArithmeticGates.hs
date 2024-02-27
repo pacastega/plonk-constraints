cdcf853a2eb477fd3c208e951157f025a6f45d75
Pablo <p.cas.gar@gmail.com>
2024-02-27 10:40:00 +0100

    Generalise arithmetic gates to `PrimitiveRoot` fields

--- src/ArithmeticGates.hs
+++ src/ArithmeticGates.hs
@@ -1,10 +1,11 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DataKinds #-}
 {-@ embed GHC.Num.Natural.Natural as int @-}
 module ArithmeticGates (addGate, mulGate) where
 
 import Constraints
--- import GHC.TypeNats (KnownNat)
--- import PrimitiveRoot
+import GHC.TypeNats (KnownNat)
+import PrimitiveRoot
 import Data.Vector (fromList)
 -- import RefinementTypes
 
@@ -15,15 +16,15 @@ type F17 = F 17
 -- annotation below has (F 17) instead of F17.
 -- https://github.com/ucsd-progsys/liquidhaskell/issues/2080
 
-{-@ addGate :: Circuit F17 1 3 @-}
-addGate :: Circuit (F 17)
+{-@ addGate :: Circuit (F p) 1 3 @-}
+addGate :: (KnownNat p, PrimitiveRoot (F p)) => Circuit (F p)
 addGate = (v, q) where
   v = let f=fromList in (f [0], f [1], f [2])
   q = let f=fromList in (f [1], f [1], f [-1], f [0], f [0])
   -- a+b == c <=> a + b - c + 0*m + 0 == 0
 
-{-@ mulGate :: Circuit F17 1 3 @-}
-mulGate :: Circuit (F 17)
+{-@ mulGate :: Circuit (F p) 1 3 @-}
+mulGate :: (KnownNat p, PrimitiveRoot (F p)) => Circuit (F p)
 mulGate = (v, q) where
   v = let f=fromList in (f [0], f [1], f [2])
   q = let f=fromList in (f [0], f [0], f [-1], f [1], f [0])
