a1595e1c14cbfa0ca22182b5dda1e63c9fb6cad3
Pablo <p.cas.gar@gmail.com>
2024-04-10 16:05:19 +0200

    Proof base cases for the compiler correctness

--- src/CompilerProof.hs
+++ src/CompilerProof.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS -Wno-unused-matches #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-@ LIQUID "--reflection" @-}
 {-@ LIQUID "--ple" @-}
@@ -6,32 +7,31 @@ module CompilerProof where
 
 import Vec
 import Constraints
-import ArithmeticGates
 import Circuits
-import Utils
+import Utils -- needed for the reflection
 
 import GHC.TypeNats (KnownNat)
-import Data.FiniteField.PrimeField (PrimeField)
-import PrimitiveRoot
 
 import DSL
 
 import Language.Haskell.Liquid.ProofCombinators
 
-{-@ compileProof :: m:Nat1 ->
+{-@ compileProof :: m:{v:Int | v >= 3} ->
                     program:LDSL p (Btwn Int 0 m) ->
                     input:VecN (F p) m ->
                     {semanticsAreCorrect m program input <=>
                      satisfies (nGates program) m input (compile m program)} @-}
-compileProof :: (KnownNat p, PrimitiveRoot (F p)) =>
+compileProof :: KnownNat p =>
                 Int -> LDSL p Int -> Vec (F p) -> Proof
 compileProof m (LWIRE i) input
   =   semanticsAreCorrect m (LWIRE i) input
   === True
   === satisfies 0 m input (emptyCircuit m)
-  === satisfies 0 m input (fst' $ compile' m (LWIRE i))
-  === satisfies 0 m input (compile m (LWIRE i))
   *** QED
-compileProof m (LCONST x i)   input = undefined
+compileProof m (LCONST x i) input
+  =   semanticsAreCorrect m (LCONST x i) input
+  === input!i == x
+  === satisfies 1 m input (constGate m x i)
+  *** QED
 compileProof m (LADD p1 p2 i) input = undefined
 compileProof m (LMUL p1 p2 i) input = undefined
