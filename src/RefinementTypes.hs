21dc776fa260a07dfeb1710e6c813e22491c4b78
Pablo <p.cas.gar@gmail.com>
2024-04-12 15:41:20 +0200

    Add definition of type refinement `ListN`

--- src/RefinementTypes.hs
+++ src/RefinementTypes.hs
@@ -9,6 +9,7 @@ import Vec
 
 {-@ type Nat1 = {v:Nat | v >= 1} @-}
 
+{-@ type ListN a N = {v:[a] | len v = N} @-}
 {-@ type VectorN a N = {v:Vector a | vlen v == N} @-}
 {-@ type VecN a N = {v:Vec a | vvlen v == N} @-}
 {-@ type Btwn t A B = {v:t | A <= v && v < B} @-} -- [A..B)
