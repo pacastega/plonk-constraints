c06577826608ce58c92321550cf121fd8175db98
Pablo <p.cas.gar@gmail.com>
2024-05-10 13:24:32 +0200

    Add some examples with boolean computations

--- app/Main.hs
+++ app/Main.hs
@@ -22,8 +22,8 @@ type F17 = PrimeField 17
 type V17 = Vec F17
 
 
-{-@ testProgram :: v:DSL _ (Btwn 0 7) @-}
-testProgram :: DSL F17 Int
+{-@ testProgram :: v:DSL _ (Btwn 0 7) _ @-}
+testProgram :: DSL F17 Int F17
 testProgram = ADD (ADD (WIRE 0) (WIRE 1)) (ADD (WIRE 2) (WIRE 3))
 
 {-@ testInput :: VecN _ 7 @-}
@@ -39,8 +39,8 @@ testInput' = witnessGen 7 (label 7 testProgram) valuation where
   valuation = const 1
 
 
-{-@ testProgram2 :: v:DSL _ (Btwn 0 7) @-}
-testProgram2 :: DSL F17 Int
+{-@ testProgram2 :: v:DSL _ (Btwn 0 7) _ @-}
+testProgram2 :: DSL F17 Int F17
 testProgram2 = MUL (ADD (WIRE 0) (CONST 15)) (ADD (WIRE 1) (CONST 3))
 
 {-@ testInput2 :: VecN _ 7 @-}
@@ -56,14 +56,34 @@ testInput2' = witnessGen 7 (label 7 testProgram2) valuation where
   valuation = \case 0 -> 7; 1 -> 3; _ -> 0
 
 
-{-@ testProgram3 :: DSL _ (Btwn 0 6) @-}
-testProgram3 :: DSL F17 Int
+{-@ testProgram3 :: DSL _ (Btwn 0 6) _ @-}
+testProgram3 :: DSL F17 Int F17
 testProgram3 = DIV (WIRE 5) (WIRE 1)
 
 {-@ testInput3 :: VecN _ 6 @-}
 testInput3 :: V17
 testInput3 = witnessGen 6 (label 6 testProgram3) valuation where
   valuation = \case 5 -> 3; 1 -> 9; _ -> 0
+
+
+{-@ testProgram4 :: DSL _ (Btwn 0 20) _ @-}
+testProgram4 :: DSL F17 Int Bool
+testProgram4 = ISZERO (WIRE 0) `OR` (NOT (ISZERO (WIRE 0)) `AND` (ISZERO (WIRE 1)))
+
+{-@ testInput4 :: VecN _ 20 @-}
+testInput4 :: V17
+testInput4 = witnessGen 20 (label 20 testProgram4) valuation where
+  valuation = \case 0 -> 0; 1 -> 3; _ -> 7
+
+{-@ testInput4' :: VecN _ 20 @-}
+testInput4' :: V17
+testInput4' = witnessGen 20 (label 20 testProgram4) valuation where
+  valuation = \case 0 -> 1; 1 -> 0; _ -> 7
+
+{-@ testInput4'' :: VecN _ 20 @-}
+testInput4'' :: V17
+testInput4'' = witnessGen 20 (label 20 testProgram4) valuation where
+  valuation = \case 0 -> 1; 1 -> 8; _ -> 7
 
 
 green :: String -> String
@@ -73,8 +93,8 @@ red :: String -> String
 red s = "\ESC[31m" ++ s ++ "\ESC[0m"
 
 
-{-@ test :: m:{v:Int | v >= 3} -> DSL _ (Btwn 0 m) -> VecN _ m -> IO () @-}
-test :: Int -> DSL F17 Int -> V17 -> IO ()
+{-@ test :: m:{v:Int | v >= 3} -> DSL _ (Btwn 0 m) t -> VecN _ m -> IO () @-}
+test :: Int -> DSL F17 Int t -> V17 -> IO ()
 test m program input = do
   let labeledProgram = label m program
   let circuit = compile m labeledProgram
@@ -95,6 +115,8 @@ test m program input = do
   putStrLn $ if semantics_ == satisfies_
     then green "SUCCESS!" else red "FAILURE"
 
+  putStrLn $ "The final result is " ++ show (input ! outputWire labeledProgram)
+
   putStrLn $ replicate 80 '='
 
 
@@ -105,3 +127,7 @@ main = do
   test 7 testProgram2 testInput2
   test 7 testProgram2 testInput2'
   test 6 testProgram3 testInput3
+
+  test 20 testProgram4 testInput4
+  test 20 testProgram4 testInput4'
+  test 20 testProgram4 testInput4''
