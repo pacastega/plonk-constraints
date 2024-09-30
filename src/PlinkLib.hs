4e1f01d76ad280a310dfdf901434bc40fc202ace
Pablo Castellanos <p.cas.gar@gmail.com>
2024-09-30 16:49:31 +0200

    Implement integers modulo 2^n as vectors of bits

--- src/PlinkLib.hs
+++ src/PlinkLib.hs
@@ -4,6 +4,7 @@
 module PlinkLib where
 
 import DSL
+import Utils (pow)
 
 -- Bit-wise applications -------------------------------------------------------
 {-@ bitwise :: op:({x:DSL p i | unpacked x} ->
@@ -17,6 +18,12 @@ bitwise _  (NIL)       (NIL)       = NIL
 bitwise op (CONS x xs) (CONS y ys) = op x y `CONS` bitwise op xs ys
 
 -- List-like functions ---------------------------------------------------------
+{-@ fromList :: l:[{v:DSL p i | unpacked v}] ->
+               {v:DSL p i | isVector v && vlength v = len l} @-}
+fromList :: [DSL p i] -> DSL p i
+fromList []     = NIL
+fromList (x:xs) = x `CONS` fromList xs
+
 {-@ vAppend :: u:{DSL p i | isVector u} ->
                v:{DSL p i | isVector v} ->
                {w:DSL p i | isVector w && vlength w = vlength u + vlength v} @-}
@@ -36,6 +43,15 @@ vTakeDrop n (CONS x xs) = let (ys, zs) = vTakeDrop (n-1) xs in (CONS x ys, zs)
 vReplicate :: Int -> DSL p i -> DSL p i
 vReplicate 0 _ = NIL
 vReplicate n p = CONS p (vReplicate (n-1) p)
+
+{-@ vZip :: u:{DSL p i | isVector u} ->
+            v:{DSL p i | isVector v && vlength v = vlength u} ->
+            w:{[({x:DSL p i | unpacked x},
+                 {y:DSL p i | unpacked y})] |
+                 len w = vlength u} @-}
+vZip :: DSL p i -> DSL p i -> [(DSL p i, DSL p i)]
+vZip (NIL)       (NIL)       = []
+vZip (CONS x xs) (CONS y ys) = (x,y) : vZip xs ys
 
 -- Shift & rotate --------------------------------------------------------------
 {-@ rotateL :: u:{DSL p i | isVector u} -> Btwn 0 (vlength u) ->
@@ -59,3 +75,40 @@ shiftL xs n = let (_, zs) = vTakeDrop n xs in
 shiftR :: Num p => DSL p i -> Int -> DSL p i
 shiftR xs n = let (ys, _) = vTakeDrop (vlength xs - n) xs in
   vReplicate n (CONST 0) `vAppend` ys
+
+-- Integers mod 2^n -----------------------------------------------------------
+{-@ fromInt :: n:Nat -> x:Btwn 0 (pow 2 n) ->
+              {v:DSL p i | isVector v && vlength v = n} @-}
+fromInt :: Num p => Int -> Int -> DSL p i
+fromInt n = go 0 NIL where
+  {-@ go :: m:{Nat | m <= n} ->
+            {acc:DSL p i | isVector acc && vlength acc = m} ->
+            x:Btwn 0 (pow 2 n) ->
+            {v:DSL p i | isVector v && vlength v = n} / [n-m] @-}
+  go :: Num p => Int -> DSL p i -> Int -> DSL p i
+  go m acc x
+    | m == n    = acc
+    | otherwise = let (q, r) = divMod x 2; r' = fromIntegral r
+                  in go (m+1) (CONST r' `CONS` acc) q
+
+
+-- FIXME: this is VERY inefficient:
+-- 1. the (double) addition ‘x+y+c’ gets computed 4 times
+-- 2. the comparisons against constants use 3 constraints, instead of 2
+-- 3. the comparison ‘sum == 3’ gets computed twice
+{-@ vecAdd :: u:{DSL p i | isVector u} ->
+              v:{DSL p i | isVector v && vlength v = vlength u} ->
+              w:{DSL p i | isVector w && vlength w >= 0} @-}
+  -- TODO: it also preserves length
+vecAdd :: Num p => DSL p i -> DSL p i -> DSL p i
+vecAdd u v = fromList $ fst $ foldr addWithCarry ([], CONST 0) (vZip u v)
+  where
+    {-@ addWithCarry :: ({x:DSL p i | unpacked x}, {y:DSL p i | unpacked y})
+                     -> ([{v:DSL p i | unpacked v}], {v:DSL p i | unpacked v})
+                     -> ([{v:DSL p i | unpacked v}], {v:DSL p i | unpacked v})
+    @-}
+    addWithCarry :: Num p => (DSL p i, DSL p i) -> ([DSL p i], DSL p i) ->
+                             ([DSL p i], DSL p i)
+    addWithCarry (x, y) (acc, carry) = let sum = (x `ADD` y) `ADD` carry in
+      ((sum `EQL` CONST 1) `OR` (sum `EQL` CONST 3) : acc, -- new acc
+       (sum `EQL` CONST 2) `OR` (sum `EQL` CONST 3))       -- new carry
