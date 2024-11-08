1001ebf810869f3ea1402ec1d01590ca942f522a
Pablo Castellanos <p.cas.gar@gmail.com>
2024-11-08 10:45:57 +0100

    Clean unused modules

--- src/RefinementTypes.hs
+++ src/RefinementTypes.hs
@@ -1,13 +1,8 @@
 {-@ LIQUID "--reflection" @-}
 
-{-# OPTIONS -Wno-unused-imports -Wno-missing-export-lists #-}
 module RefinementTypes where
-
-
-import Data.Vector
 
 {-@ type Nat1 = {v:Nat | v >= 1} @-}
 
 {-@ type ListN a N = {v:[a] | len v = N} @-}
-{-@ type VectorN a N = {v:Vector a | vlen v == N} @-}
 {-@ type Btwn A B = {v:Int | A <= v && v < B} @-} -- [A..B)
