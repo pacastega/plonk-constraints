2456b4f65810b902d63145ed38092e4cf85e1fc9
Pablo Castellanos <p.cas.gar@gmail.com>
2024-11-11 09:48:51 +0100

    Simplify type alias definition for `Nat1`

--- src/TypeAliases.hs
+++ src/TypeAliases.hs
@@ -2,7 +2,7 @@
 
 module TypeAliases where
 
-{-@ type Nat1 = {v:Nat | v >= 1} @-}
+{-@ type Nat1 = {v:Int | v >= 1} @-}
 
 {-@ type ListN a N = {v:[a] | len v = N} @-}
 {-@ type Btwn A B = {v:Int | A <= v && v < B} @-} -- [A..B)
