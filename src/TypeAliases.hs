36b610cb91ff2c0eb52581022a2b693e00f0fcc2
Pablo Castellanos <p.cas.gar@gmail.com>
2025-02-14 12:47:27 +0100

    Fix `compilerProof` not passing

--- src/TypeAliases.hs
+++ src/TypeAliases.hs
@@ -1,7 +1,7 @@
 {-@ LIQUID "--reflection" @-}
 
 module TypeAliases where
--- import GHC.Num.Integer -- IS
+import GHC.Num.Integer (Integer (IS))
 
 {-@ type Nat1 = {v:Int | v >= 1} @-}
 
@@ -11,7 +11,7 @@ module TypeAliases where
 -- tries to fix 9971f17cde80c9cfe0b895f8f0204db96f16dafb
 {-@ define fromInteger x = (x) @-}
 -- {-@ define fromIntegral x = (x) @-}
--- {-@ define IS x = (x) @-}
+{-@ define IS x = (x) @-}
 -- {-@ define div x y = (x / y) @-}
 -- {-@ define (/) x y = (x / y) @-}
 -- {-@ define mod x y = (x mod y) @-}
