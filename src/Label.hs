ab9b101a8b56220e56de8d302ea7985e695c1637
Pablo Castellanos <p.cas.gar@gmail.com>
2025-03-21 12:49:35 +0100

    Revert "Try to improve fix for CSE bug in equality assertion"
    This reverts commit 57f2c0dfa27222e5c5bb4e1945721254008bc000.

--- src/Label.hs
+++ src/Label.hs
@@ -19,7 +19,7 @@ type Env p i = M.Map (DSL p) i
 label :: (Num p, Ord p) => DSL p -> Store p -> (Int, [LDSL p Int], [LDSL p Int])
 label program store = (m, labeledPrograms, labeledStore) where
   (m', labeledStore, env') = labelStore store 0 M.empty
-  (m, labeledPrograms, _) = label' program m' env' Nothing
+  (m, labeledPrograms, _) = label' program m' env'
 
 {-@ labelStore :: Store p -> m0:Nat -> Env p (Btwn 0 m0)
                -> (m:{Int | m >= m0}, [LDSL p Int], Env p Int)
@@ -45,8 +45,8 @@ label2 :: (Num p, Ord p) => Int -> DSL p -> DSL p -> Env p Int ->
           (Int, LDSL p Int, LDSL p Int, Env p Int)
 label2 nextIndex arg1 arg2 env =
   let i = nextIndex
-      (i1, [arg1'], env1) = label' arg1 i  env  Nothing
-      (i2, [arg2'], env2) = label' arg2 i1 env1 Nothing
+      (i1, [arg1'], env1) = label' arg1 i  env
+      (i2, [arg2'], env2) = label' arg2 i1 env1
   in (i2, arg1', arg2', env2)
 
 
@@ -57,64 +57,63 @@ add (k,v) = M.alter (\_ -> Just v) k
 {-@ lazy label' @-}
 {-@ label' :: program:TypedDSL p ->
               m0:Nat -> Env p (Btwn 0 m0) ->
-              Maybe (Btwn 0 m0) ->
               (m:{Int | m >= m0}, [LDSL p Int], Env p Int)
            <\m   -> {l:[LDSL p (Btwn 0 m)] | scalar program => len l = 1},
             \_ m -> {v:Env   p (Btwn 0 m)  | true}> @-}
-label' :: (Num p, Ord p) => DSL p -> Int -> Env p Int -> Maybe Int
+label' :: (Num p, Ord p) => DSL p -> Int -> Env p Int
        -> (Int, [LDSL p Int], Env p Int)
-label' p nextIndex env requested = case M.lookup p env of
+label' p nextIndex env = case M.lookup p env of
   Just i  -> (nextIndex, [LWIRE i], env)
   Nothing -> let i = nextIndex in case p of
 
-    VAR s _ -> withOutputWire i requested p (LVAR s) env
-    CONST x -> withOutputWire i requested p (LCONST x) env
-    BOOLEAN b  -> label' (CONST $ fromIntegral $ fromEnum b) nextIndex env requested
+    VAR s _ -> (i+1, [LVAR s i], add (p,i) env)
+    CONST x -> (i+1, [LCONST x i], add (p,i) env)
+    BOOLEAN b  -> label' (CONST $ fromIntegral $ fromEnum b) nextIndex env
 
-    ADD p1 p2 -> withOutputWire i' requested p (LADD p1' p2') env
+    ADD p1 p2 -> (i'+1, [LADD p1' p2' i'], add (p,i') env')
       where (i', p1', p2', env') = label2 i p1 p2 env
-    SUB p1 p2 -> withOutputWire i' requested p (LSUB p1' p2') env
+    SUB p1 p2 -> (i'+1, [LSUB p1' p2' i'], add (p,i') env')
       where (i', p1', p2', env') = label2 i p1 p2 env
-    MUL p1 p2 -> withOutputWire i' requested p (LMUL p1' p2') env
+    MUL p1 p2 -> (i'+1, [LMUL p1' p2' i'], add (p,i') env')
       where (i', p1', p2', env') = label2 i p1 p2 env
-    DIV p1 p2 -> withOutputWire i' requested p (LDIV p1' p2' w') env
-      where (w', p1', p2', env') = label2 i p1 p2 env; i' = w'+1
-    ADDC p1 k -> withOutputWire i' requested p (LADDC p1' k) env
-      where (i', [p1'], env') = label' p1 i env Nothing
-    MULC p1 k -> withOutputWire i' requested p (LMULC p1' k) env
-      where (i', [p1'], env') = label' p1 i env Nothing
-    LINCOMB k1 p1 k2 p2 -> withOutputWire i' requested p (LLINCOMB k1 p1' k2 p2') env
-      where (i', p1', p2', env') = label2 i p1 p2 env
-
-    NOT p1    -> withOutputWire i' requested p (LNOT p1') env
-      where (i', [p1'], env') = label' p1 i env Nothing
-    AND p1 p2 -> withOutputWire i' requested p (LAND p1' p2') env
-      where (i', p1', p2', env') = label2 i p1 p2 env
-    OR  p1 p2 -> withOutputWire i' requested p (LOR p1' p2') env
-      where (i', p1', p2', env') = label2 i p1 p2 env
-    XOR p1 p2 -> withOutputWire i' requested p (LXOR p1' p2') env
+    DIV p1 p2 -> (w'+1, [LDIV p1' p2' w' i'], add (p,i') env')
+      where (i', p1', p2', env') = label2 i p1 p2 env; w' = i'+1
+    ADDC p1 k -> (i'+1, [LADDC p1' k i'], add (p,i') env')
+      where (i', [p1'], env') = label' p1 i env
+    MULC p1 k -> (i'+1, [LMULC p1' k i'], add (p,i') env')
+      where (i', [p1'], env') = label' p1 i env
+    LINCOMB k1 p1 k2 p2 -> (i'+1, [LLINCOMB k1 p1' k2 p2' i'], add (p,i') env')
       where (i', p1', p2', env') = label2 i p1 p2 env
 
-    UnsafeNOT p1    -> withOutputWire i' requested p (LUnsafeNOT p1') env
-      where (i', [p1'], env') = label' p1 i env Nothing
-    UnsafeAND p1 p2 -> withOutputWire i' requested p (LUnsafeAND p1' p2') env
+    NOT p1    -> (i'+1, [LNOT p1' i'], add (p,i') env')
+      where (i', [p1'], env') = label' p1 i env
+    AND p1 p2 -> (i'+1, [LAND p1' p2' i'], add (p,i') env')
       where (i', p1', p2', env') = label2 i p1 p2 env
-    UnsafeOR  p1 p2 -> withOutputWire i' requested p (LUnsafeOR  p1' p2') env
+    OR  p1 p2 -> (i'+1, [LOR  p1' p2' i'], add (p,i') env')
       where (i', p1', p2', env') = label2 i p1 p2 env
-    UnsafeXOR p1 p2 -> withOutputWire i' requested p (LUnsafeXOR p1' p2') env
+    XOR p1 p2 -> (i'+1, [LXOR p1' p2' i'], add (p,i') env')
       where (i', p1', p2', env') = label2 i p1 p2 env
 
-    ISZERO p1 -> label' (EQLC p1 0) nextIndex env Nothing
-    EQL p1 p2 -> label' (ISZERO (p1 `SUB` p2)) nextIndex env Nothing
-    EQLC p1 k -> withOutputWire i' requested p (LEQLC p1' k w') env
-      where (w', [p1'], env') = label' p1 i env Nothing; i' = w'+1
+    UnsafeNOT p1    -> (i'+1, [LUnsafeNOT p1' i'], add (p,i') env')
+      where (i', [p1'], env') = label' p1 i env
+    UnsafeAND p1 p2 -> (i'+1, [LUnsafeAND p1' p2' i'], add (p,i') env')
+      where (i', p1', p2', env') = label2 i p1 p2 env
+    UnsafeOR  p1 p2 -> (i'+1, [LUnsafeOR  p1' p2' i'], add (p,i') env')
+      where (i', p1', p2', env') = label2 i p1 p2 env
+    UnsafeXOR p1 p2 -> (i'+1, [LUnsafeXOR p1' p2' i'], add (p,i') env')
+      where (i', p1', p2', env') = label2 i p1 p2 env
+
+    ISZERO p1 -> label' (EQLC p1 0) nextIndex env
+    EQL p1 p2 -> label' (ISZERO (p1 `SUB` p2)) nextIndex env
+    EQLC p1 k -> (w'+1, [LEQLC p1' k w' i'], add (p,i') env')
+      where (i', [p1'], env') = label' p1 i env; w' = i'+1
 
     NIL _ -> (i, [], env)
     CONS h ts -> (i'', h' ++ ts', env'')
-      where (i',  h',  env')  = label' h  i  env Nothing
-            (i'', ts', env'') = label' ts i' env' Nothing
+      where (i',  h',  env')  = label' h  i  env
+            (i'', ts', env'') = label' ts i' env'
 
-    BoolToF p -> label' p i env Nothing -- noop
+    BoolToF p -> label' p i env -- noop
 
 {-@ lazy labelStore' @-}
 {-@ labelStore' :: assertion:(Assertion p) ->
@@ -126,36 +125,56 @@ labelStore' :: (Num p, Ord p) => Assertion p -> Int -> Env p Int
             -> (Int, [LDSL p Int], Env p Int)
 labelStore' assertion nextIndex env = let i = nextIndex in case assertion of
     DEF s d τ -> (i', [d'], add (VAR s τ, outputWire d') env')
-      where (i', [d'], env') = label' d i env Nothing
+      where (i', [d'], env') = label' d i env
     NZERO p1  -> (w'+1, [LNZERO p1' w'], env')
-      where (w', [p1'], env') = label' p1 i env Nothing
+      where (w', [p1'], env') = label' p1 i env
     BOOL p1  -> (i', [LBOOL p1'], env')
-      where (i', [p1'], env') = label' p1 i env Nothing
+      where (i', [p1'], env') = label' p1 i env
     EQA p1 p2 -> case M.lookup p1 env of
       Just i1 -> case M.lookup p2 env of
         Just i2 -> (i, [LEQA (LWIRE i1) (LWIRE i2)], env) -- both present
-        Nothing -> (i', [p2'], env')
-          where (i', [p2'], env') = label' p2 i env (Just i1) -- use i1 for p2
+        Nothing -> (i', [withOutputWire i' i1 p2'], env'') -- use i1 for p2
+          where (i', [p2'], env') = label' p2 i env
+                env'' = add (p2, i1) env'
       Nothing -> case M.lookup p2 env of
-        Just i2 -> (i', [p1'], env')
-          where (i', [p1'], env') = label' p1 i env (Just i2) -- use i2 for p1
-        Nothing -> (i'', [p1', p2'], env'')
-          where (i', [p1'], env') = label' p1 i  env  Nothing
+        Just i2 -> (i', [withOutputWire i' i2 p1'], env'')
+          where (i', [p1'], env') = label' p1 i env       -- use i2 for p1
+                env'' = add (p1, i2) env'
+        Nothing -> (i', [p1', withOutputWire i' i1 p2'], env'')
+          where (i', p1', p2', env') = label2 i p1 p2 env
                 i1 = outputWire p1'
-                (i'', [p2'], env'') = label' p2 i' env' (Just i1)
+                env'' = add (p2, i1) env' -- arbitrarily choose i1 for both
 
 
+{-@ withOutputWire :: m:Nat -> Btwn 0 m -> LDSL p (Btwn 0 m)
+                   -> LDSL p (Btwn 0 m) @-}
+withOutputWire :: Int -> Int -> LDSL p Int -> LDSL p Int
+withOutputWire _ i program = case program of
+  LWIRE _ -> LWIRE i
+  LVAR s _ -> LVAR s i
+  LCONST p _ -> LCONST p i
 
-{-@ withOutputWire :: m0:Nat -> requested:Maybe (Btwn 0 m0)
-                   -> program:TypedDSL p
-                   -> program':({v:Nat | v <= m0} -> LDSL p {v:Nat | v <= m0})
-                   -> Env p (Btwn 0 m0)
-                   -> (m:{Int | m >= m0}, [LDSL p Int], Env p Int)
-                <\m   -> {l:[LDSL p (Btwn 0 m)] | len l = 1},
-                 \_ m -> {v:Env   p (Btwn 0 m)  | true}> @-}
-withOutputWire :: Ord p => Int -> Maybe Int
-               -> DSL p -> (Int -> LDSL p Int) -> Env p Int
-               -> (Int, [LDSL p Int], Env p Int)
-withOutputWire i requested program program' env = case requested of
-  Just j  -> (i,   [program' j], add (program,j) env) -- use requested index
-  Nothing -> (i+1, [program' i], add (program,i) env) -- use the next available
+  LADD p1 p2 _ -> LADD p1 p2 i
+  LSUB p1 p2 _ -> LSUB p1 p2 i
+  LMUL p1 p2 _ -> LMUL p1 p2 i
+  LDIV p1 p2 w _ -> LDIV p1 p2 w i
+
+  LADDC p k _ -> LADDC p k i
+  LMULC p k _ -> LMULC p k i
+  LLINCOMB k1 p1 k2 p2 _ -> LLINCOMB k1 p1 k2 p2 i
+
+  LNOT p1 _ -> LNOT p1 i
+  LAND p1 p2 _ -> LAND p1 p2 i
+  LOR  p1 p2 _ -> LOR  p1 p2 i
+  LXOR p1 p2 _ -> LXOR p1 p2 i
+
+  LUnsafeNOT p1 _ -> LUnsafeNOT p1 i
+  LUnsafeAND p1 p2 _ -> LUnsafeAND p1 p2 i
+  LUnsafeOR  p1 p2 _ -> LUnsafeOR  p1 p2 i
+  LUnsafeXOR p1 p2 _ -> LUnsafeXOR p1 p2 i
+
+  LEQLC   p1 k w _ -> LEQLC p1 k w i
+
+  LNZERO p1 _ -> LNZERO p1 i
+  LBOOL  p1 -> LBOOL p1
+  LEQA p1 p2 -> LEQA p1 p2
