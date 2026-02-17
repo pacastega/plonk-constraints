e56ebe1586d8540024aee5beb64e7e7b14166fd2
Pablo Castellanos <p.cas.gar@gmail.com>
2026-02-17 12:33:02 +0100

    Prove witness generation completeness (variable case)

---
+++ src/WitnessGenProof/CompletenessVar.hs
@@ -0,0 +1,83 @@
+{-# OPTIONS_GHC -Wno-redundant-constraints #-}
+{-# LANGUAGE CPP #-}
+{-@ LIQUID "--reflection" @-}
+{-@ LIQUID "--ple" @-}
+{-@ LIQUID "--ple-with-undecided-guards" @-}
+
+{-@ LIQUID "--cores=1" @-}
+
+module WitnessGenProof.CompletenessVar where
+
+#if LiquidOn
+import qualified Liquid.Data.Map as M
+#else
+import qualified Data.Map as M
+import qualified MapFunctions as M
+#endif
+
+import Utils
+import TypeAliases
+
+import Constraints
+import DSL
+import Label
+import WitnessGeneration
+import Semantics
+
+import MapLemmas
+import LabelingProof.LabelingLemmas
+
+import Language.Haskell.Liquid.ProofCombinators
+
+
+{-@ wgCompleteVar :: m0:Nat -> m:{Nat | m >= m0}
+                  -> s:Var
+                  -> τ:ScalarTy
+                  -> ρ:{NameValuation p | isJust (eval (VAR s τ) ρ)}
+                  -> λ:LabelEnv p (Btwn 0 m0)
+                  -> σ:WireValuation p m0
+
+                  -> Composable ρ λ σ
+
+                  -> λ':LabelEnv p (Btwn 0 m)
+                  -> e':{LDSL p (Btwn 0 m) | freshE e' σ
+                                 && label' (VAR s τ) m0 λ = (m, mkList1 e', λ')}
+
+                  -> { isJust (witnessGenE' m ρ σ e') } @-}
+wgCompleteVar :: (Fractional p, Eq p, Ord p)
+              => Int -> Int -> Var -> Ty
+              -> NameValuation p -> LabelEnv p Int -> WireValuation p
+
+              -> (Var -> Proof)
+
+              -> LabelEnv p Int
+              -> LDSL p Int
+
+              -> Proof
+wgCompleteVar m0 m s τ ρ λ σ π λ' e' = case M.lookup s ρ of
+  Nothing -> error "the variable is defined in ρ because eval succeeds"
+  Just value -> case M.lookup s λ of -- Is the variable labeled already?
+    Nothing -> case τ of             -- NO: we get "LVAR s τ i"
+      TF -> trivial
+      TBool -> if boolean value
+               then trivial
+               else error "the value is in {0,1} because eval succeeds"
+    Just j -> elementLemma s j λ     -- YES: we get "LWIRE j"
+            ? π s ? lookupLemma s λ
+            ? case τ of
+                TF -> witnessGenE' m ρ σ (LWIRE τ j) === Just σ *** QED
+                TBool -> if boolean value
+                         then witnessGenE' m ρ σ (LWIRE τ j) === Just σ *** QED
+                         else error "the value is in {0,1} because eval succeeds"
+
+-- workarounds to fix "crash: unknown constant" --------------------------------
+
+{-@ reflect foo @-}
+foo :: UnOp Int -> Int
+foo (ADDC x) = x
+foo _        = 0
+
+{-@ reflect barOp @-}
+barOp :: BinOp Int -> Int
+barOp ADD = 0
+barOp _   = 1
