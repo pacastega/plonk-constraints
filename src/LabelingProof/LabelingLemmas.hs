1bc057c82e8e1fc2b5bab5b74cfe8bcc6ebf6312
Pablo Castellanos <p.cas.gar@gmail.com>
2026-02-18 16:23:55 +0100

    Format label2Inc

--- src/LabelingProof/LabelingLemmas.hs
+++ src/LabelingProof/LabelingLemmas.hs
@@ -38,11 +38,18 @@ label1Inc op _ _ _ _ _ _ _ _ _ = case op of
   EQLC _  -> ()
   _       -> ()
 
-{-@ label2Inc :: op:{BinOp p | desugaredBinOp op || op == EQL || op == DIV } -> e1:DSL p -> e2:{DSL p | wellTyped (BIN op e1 e2)} -> m0:Nat -> λ:LabelEnv p (Btwn 0 m0)
-              -> m1:Int -> e1':LDSL p (Btwn 0 m1) -> λ1:{LabelEnv p (Btwn 0 m1) | label' e1 m0 λ  = (m1, mkList1 e1', λ1)}
-              -> m2:Int -> e2':LDSL p (Btwn 0 m2) -> λ2:{LabelEnv p (Btwn 0 m2) | label' e2 m1 λ1 = (m2, mkList1 e2', λ2)}
-              ->  m:Int ->  e':LDSL p Int -> λ':{LabelEnv p Int | label' (BIN op e1 e2) m0 λ = (m, mkList1 e', λ')}
-              -> ({m2 >= m1}) @-}
+{-@ label2Inc :: op:BinOp p -> e1:DSL p -> e2:{DSL p | wellTyped (BIN op e1 e2)}
+              -> m0:Nat -> λ:LabelEnv p (Btwn 0 m0)
+
+              -> m1:Nat -> e1':LDSL p (Btwn 0 m1)
+              -> λ1:{LabelEnv p (Btwn 0 m1) | label' e1 m0 λ  = (m1, mkList1 e1', λ1)}
+
+              -> m2:Nat -> e2':LDSL p (Btwn 0 m2)
+              -> λ2:{LabelEnv p (Btwn 0 m2) | label' e2 m1 λ1 = (m2, mkList1 e2', λ2)}
+
+              ->  m:Nat ->  e':LDSL p (Btwn 0 m)
+              -> λ':{LabelEnv p (Btwn 0 m) | label' (BIN op e1 e2) m0 λ = (m, mkList1 e', λ')}
+              -> {m2 >= m1} @-}
 label2Inc :: (Num p, Ord p) => BinOp p -> DSL p -> DSL p -> Int -> LabelEnv p Int
           -> Int -> LDSL p Int -> LabelEnv p Int
           -> Int -> LDSL p Int -> LabelEnv p Int
