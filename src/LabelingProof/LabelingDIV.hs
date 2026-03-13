568f4e1a560b6c79aef04aec8f11725f843accf7
Pablo Castellanos <p.cas.gar@gmail.com>
2026-03-13 10:10:54 +0100

    Small fix in LabelingDiv

--- src/LabelingProof/LabelingDIV.hs
+++ src/LabelingProof/LabelingDIV.hs
@@ -42,9 +42,9 @@ import Language.Haskell.Liquid.ProofCombinators
                   -> p1':{LDSL p (Btwn 0 m1) | label' p1 m0 λ  = (m1, mkList1 p1', λ1)}
                   -> p2':{LDSL p (Btwn 0 m2) | label' p2 m1 λ1 = (m2, mkList1 p2', λ2)}
                   -> e':{LDSL p (Btwn 0 m) | label' (BIN DIV p1 p2) m0 λ = (m, mkList1 e', λ')}
-                  -> σ':{WireValuation p m | Just σ' = witnessGenE' m ρ σ e'}
-                  -> σ1:{WireValuation p m1 | Just σ1 = witnessGenE' m ρ σ p1'}
-                  -> σ2:{WireValuation p m2 | Just σ2 = witnessGenE' m ρ σ1 p2'}
+                  -> σ':{WireValuation p m | Just σ' = witnessGenE' m ρ σ  e'}
+                  -> σ1:{WireValuation p m | Just σ1 = witnessGenE' m ρ σ  p1'}
+                  -> σ2:{WireValuation p m | Just σ2 = witnessGenE' m ρ σ1 p2'}
 
                   -> Agree λ2 ρ σ2
 
