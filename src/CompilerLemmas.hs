085a68fbee1c0f3a8de7b357bc275b91b08a8021
Pablo Castellanos <p.cas.gar@gmail.com>
2026-05-20 14:44:36 +0200

    Finish proof of correctness of compilation

---
+++ src/CompilerLemmas.hs
@@ -0,0 +1,216 @@
+{-# LANGUAGE CPP #-}
+{-# OPTIONS -Wno-unused-matches -Wno-unused-imports
+            -Wno-redundant-constraints #-}
+{-@ LIQUID "--reflection" @-}
+{-@ LIQUID "--ple" @-}
+module CompilerLemmas where
+
+--TODO: copy this over to BooleanProof.hs
+#if LiquidOn
+import qualified Liquid.Data.Map as M
+#else
+import qualified Data.Map as M
+import qualified MapFunctions as M
+#endif
+
+import MapLemmas
+
+import qualified Data.Set as S
+
+import Constraints
+import Circuits
+import Utils
+import TypeAliases
+
+import ArithmeticGates
+import LogicGates
+
+import DSL
+
+import BooleanProof hiding (foo, barOp)
+
+import Language.Haskell.Liquid.ProofCombinators
+
+
+{-@ booleanLemma1 :: m:Nat
+                  -> e1:{LDSL p (Btwn 0 m) | booleanE e1}
+                  -> σ:{WireValuation p m | closedExpr m σ e1}
+
+                  -> γ:TyEnv' (Btwn 0 m)
+                  -> γ1:{TyEnv' (Btwn 0 m) | Just γ1 = tyEnv'_ e1 γ}
+
+                  -> ws:{S.Set (Btwn 0 m) | wfWire'_ ws e1}
+
+                  -> π1:( j:{Btwn 0 m | S.member j ws
+                                     && M.lookup j γ1 = Just TBool}
+                            -> { boolean (M.lookup' j σ) } )
+
+                  -> { coherentE m e1 σ =>
+                       boolean (M.lookup' (outputWire e1) σ) } @-}
+booleanLemma1 :: (Fractional p, Eq p) => Int -> LDSL p Int -> WireValuation p
+
+              -> TyEnv' Int -> TyEnv' Int
+              -> S.Set Int
+              -> (Int -> Proof)
+
+              -> Proof
+booleanLemma1 m e1 σ γ γ1 ws π1 = outputWireBool e1 γ γ1 ??
+  if S.member i1 (wiresE e1)
+  then booleanProof' m σ e1 γ γ1 i1
+  else wfWireLemma ws e1 ?? π1 i1
+
+  where i1 = outputWire e1
+
+
+{-@ booleanLemma2 :: m:Nat -> op:BinOp' p
+                  -> e1:LDSL p (Btwn 0 m)
+                  -> e2:LDSL p (Btwn 0 m)
+                  -> i:Btwn 0 m
+                  -> e:{TypedLDSL p (Btwn 0 m) | e = LBIN op e1 e2 i
+                                              && booleanE e}
+                  -> σ:{WireValuation p m | closedExpr m σ e}
+
+                  -> γ:TyEnv' (Btwn 0 m)
+                  -> γ1:{TyEnv' (Btwn 0 m) | Just γ1 = tyEnv'_ e1 γ}
+                  -> γ2:{TyEnv' (Btwn 0 m) | Just γ2 = tyEnv'_ e2 γ1}
+                  -> γ':{TyEnv' (Btwn 0 m) | Just γ' = tyEnv'_ e  γ}
+
+                  -> ws:{S.Set (Btwn 0 m) | wfWire'_ ws e}
+
+                  -> π1:( j:{Btwn 0 m | S.member j ws
+                                     && M.lookup j γ1 = Just TBool}
+                            -> { boolean (M.lookup' j σ) } )
+
+                  -> π2:( j:{Btwn 0 m | S.member j (S.union ws (wiresE e1))
+                                     && M.lookup j γ2 = Just TBool}
+                            -> { coherentE m e1 σ => boolean (M.lookup' j σ) } )
+
+                  -> { coherentE m e1 σ && coherentE m e2 σ =>
+                       boolean (M.lookup' (outputWire e2) σ) } @-}
+booleanLemma2 :: (Fractional p, Eq p)
+              => Int -> BinOp p -> LDSL p Int -> LDSL p Int -> Int -> LDSL p Int
+              -> WireValuation p
+
+              -> TyEnv' Int -> TyEnv' Int -> TyEnv' Int -> TyEnv' Int
+              -> S.Set Int
+
+              -> (Int -> Proof) -> (Int -> Proof)
+
+              -> Proof
+booleanLemma2 m op e1 e2 i e σ γ γ1 γ2 γ' ws π1 π2 = outputWireBool e2 γ1 γ2 ??
+  if S.member i2 (wiresE e2)
+  then booleanProof' m σ e2 γ1 γ2 i2
+  else wfWireLemma (ws `S.union` wiresE e1) e2
+    ?? π2 i2
+
+  where i2 = outputWire e2
+
+
+{-@ reflect wfWire'_ @-}
+wfWire'_ :: (Ord i) => S.Set i -> LDSL p i -> Bool
+wfWire'_ ws e = case e of
+  LWIRE _ j -> S.member j ws -- j appears in the accumulated set of wires
+  LVAR _ _ _ -> True
+  LCONST _ _ -> True
+  LBOOL _ _ -> True
+
+  LDIV e1 e2 _ _ -> wfWire'_ ws                       e1
+                 && wfWire'_ (ws `S.union` wiresE e1) e2
+
+  LUN _ e1 _ -> wfWire'_ ws e1
+  LBIN _ e1 e2 _ -> wfWire'_ ws                       e1
+                 && wfWire'_ (ws `S.union` wiresE e1) e2
+
+  LBoolToF e1 -> wfWire'_ ws e1
+  LEQLC e1 _ _ _ -> wfWire'_ ws e1
+
+  LNIL _ -> True
+  LCONS e1 e2 -> wfWire'_ ws                       e1
+              && wfWire'_ (ws `S.union` wiresE e1) e2
+
+
+{-@ wfWireLemma :: ws:S.Set i
+                -> e:{LDSL p i | wfWire'_ ws e}
+                -> { S.isSubsetOf (wWiresE e) (S.union (wiresE e) ws) } @-}
+wfWireLemma :: (Ord i) => S.Set i -> LDSL p i -> Proof
+wfWireLemma ws e = case e of
+  LWIRE _ j -> trivial
+  LVAR _ _ _ -> trivial
+  LCONST _ _ -> trivial
+  LBOOL _ _ -> trivial
+
+  LDIV e1 e2 _ _ -> wfWireLemma ws e1
+                 ?? wfWireLemma (ws `S.union` wiresE e1) e2
+
+  LUN _ e1 _ -> wfWireLemma ws e1
+  LBIN _ e1 e2 _ -> wfWireLemma ws e1
+                 ?? wfWireLemma (ws `S.union` wiresE e1) e2
+
+  LBoolToF e1 -> wfWireLemma ws e1
+  LEQLC e1 _ _ _ -> wfWireLemma ws e1
+
+  LNIL _ -> trivial
+  LCONS e1 e2 -> wfWireLemma ws e1
+              ?? wfWireLemma (ws `S.union` wiresE e1) e2
+
+
+{-@ wiresCDistr :: n1:Nat -> n2:Nat -> n:{Nat | n = n1+n2} -> m:Nat
+                -> c1:Circuit p n1 m -> c2:Circuit p n2 m
+                -> { wiresC n m (append' c1 c2) =
+                      S.union (wiresC n1 m c1) (wiresC n2 m c2) } @-}
+wiresCDistr :: (Num p) => Int -> Int -> Int -> Int
+            -> Circuit p -> Circuit p -> Proof
+wiresCDistr n1 n2 n m []     c2 = trivial
+wiresCDistr n1 n2 n m (g:gs) c2 = wiresCDistr (n1-1) n2 (n-1) m gs c2
+
+
+{-@ compWiresLemma :: m:Nat -> e:TypedLDSL p (Btwn 0 m)
+                -> { S.isSubsetOf (wiresC (nGatesE e) m (compileE m e))
+                                  (S.union (wiresE e) (wWiresE e)) } @-}
+compWiresLemma :: (Fractional p, Eq p) => Int -> LDSL p Int -> Proof
+compWiresLemma m e = case e of
+  LWIRE _ _ -> trivial
+  LVAR _ τ _ -> case τ of
+    TF -> trivial
+    TBool -> trivial
+  LCONST _ _ -> trivial
+  LBOOL _ _ -> trivial
+
+  LDIV e1 e2 _ _ -> compWiresLemma m e1 ? compWiresLemma m e2
+                  ? wiresCDistr n1 n2 (n1+n2) m (compileE m e1) (compileE m e2)
+                  ? outputWire e1 ? outputWire e2
+    where n1 = nGatesE e1; n2 = nGatesE e2
+  LUN op e1 _ -> case op of
+    ADDC k    -> proof; MULC k    -> proof
+    NOT       -> proof; UnsafeNOT -> proof
+    where proof = compWiresLemma m e1 ? outputWire e1
+  LBIN op e1 e2 _ -> case op of
+    ADD -> proof; SUB -> proof; MUL -> proof; LINCOMB _ _ -> proof;
+
+    AND -> proof; OR  -> proof; XOR -> proof;
+    UnsafeAND -> proof; UnsafeOR  -> proof; UnsafeXOR -> proof;
+
+    where proof = compWiresLemma m e1 ? compWiresLemma m e2
+                ? wiresCDistr n1 n2 (n1+n2) m (compileE m e1) (compileE m e2)
+                ? outputWire e1 ? outputWire e2
+          n1 = nGatesE e1; n2 = nGatesE e2
+
+  LBoolToF e1 -> compWiresLemma m e1 ? outputWire e1
+  LEQLC e1 _ _ _ -> compWiresLemma m e1 ? outputWire e1
+
+  LNIL _ -> trivial
+  LCONS e1 e2 -> compWiresLemma m e1 ? compWiresLemma m e2
+               ? wiresCDistr n1 n2 (n1+n2) m (compileE m e1) (compileE m e2)
+    where n1 = nGatesE e1; n2 = nGatesE e2
+
+-- workarounds to fix "crash: unknown constant" --------------------------------
+
+{-@ reflect foo @-}
+foo :: UnOp Int -> Int
+foo (ADDC x) = x
+foo _        = 0
+
+{-@ reflect barOp @-}
+barOp :: BinOp Int -> Int
+barOp ADD = 0
+barOp _   = 1
--- src/CompilerProof.hs
+++ src/CompilerProof.hs
@@ -30,6 +30,7 @@ import LogicGates
 import DSL
 
 import BooleanProof
+import CompilerLemmas
 
 import Language.Haskell.Liquid.ProofCombinators
 
@@ -44,70 +45,19 @@ import Language.Haskell.Liquid.ProofCombinators
 -- compileProof m (LExpr e) σ = compileProofE m e σ
 -- compileProof m (LAss a) σ = compileProofA m a σ
 
-{-@ reflect wfWire'_ @-}
-wfWire'_ :: (Ord i) => S.Set i -> LDSL p i -> Bool
-wfWire'_ ws e = case e of
-  LWIRE _ j -> S.member j ws -- j appears in the accumulated set of wires
-  LVAR _ _ _ -> True
-  LCONST _ _ -> True
-  LBOOL _ _ -> True
 
-  LDIV e1 e2 _ _ -> True
-                 --    S.isSubsetOf (wWiresE e1) (ws `S.union` wiresE e1)
-                 -- && S.isSubsetOf (wWiresE e2) (ws `S.union` wiresE e1 `S.union` wiresE e2)
-                 && wfWire'_ (ws)                     e1
-                 && wfWire'_ (ws `S.union` wiresE e1) e2
-
-  LUN _ e1 _ -> wfWire'_ (ws) e1
-  LBIN _ e1 e2 _ -> wfWire'_ (ws)                     e1
-                 && wfWire'_ (ws `S.union` wiresE e1) e2
-
-  LBoolToF e1 -> wfWire'_ (ws) e1
-  LEQLC e1 _ _ _ -> wfWire'_ (ws) e1
-
-  LNIL _ -> True
-  LCONS e1 e2 -> wfWire'_ (ws)                     e1
-              && wfWire'_ (ws `S.union` wiresE e1) e2
-
-
-{-@ wfWireLemma :: ws:S.Set i
-                -> e:{LDSL p i | wfWire'_ ws e}
-                -> { S.isSubsetOf (wWiresE e) (S.union (wiresE e) ws) } @-}
-wfWireLemma :: (Ord i) => S.Set i -> LDSL p i -> Proof
-wfWireLemma ws e = case e of
-  LWIRE _ j -> trivial
-  LVAR _ _ _ -> trivial
-  LCONST _ _ -> trivial
-  LBOOL _ _ -> trivial
-
-  LDIV e1 e2 _ _ -> wfWireLemma ws e1
-                 ?? wfWireLemma (ws `S.union` wiresE e1) e2
-
-  LUN _ e1 _ -> wfWireLemma ws e1
-  LBIN _ e1 e2 _ -> wfWireLemma ws e1
-                 ?? wfWireLemma (ws `S.union` wiresE e1) e2
-
-  LBoolToF e1 -> wfWireLemma ws e1
-  LEQLC e1 _ _ _ -> wfWireLemma ws e1
-
-  LNIL _ -> trivial
-  LCONS e1 e2 -> wfWireLemma ws e1
-              ?? wfWireLemma (ws `S.union` wiresE e1) e2
-
-
-{-@ compileProofE2 :: m:Nat
+{-@ compileProofE :: m:Nat
                   -> e:{TypedLDSL p (Btwn 0 m) | isJust (tyEnv' e) && wfWire'_ S.empty e}
                   -> {σ:WireValuation p m | closedExpr m σ e}
                   -> { coherentE m e σ <=>
                         satisfies (nGatesE e) m σ (compileE m e) } @-}
-compileProofE2 :: (Eq p, Fractional p) => Int -> LDSL p Int
+compileProofE :: (Eq p, Fractional p) => Int -> LDSL p Int
               -> WireValuation p -> Proof
-compileProofE2 m e σ = case tyEnv' e of
-  Just γ -> compileProofE m e M.MTip γ σ S.empty (booleanProof' m σ e M.MTip γ)
-  -- Just γ -> compileProofE m e M.MTip γ σ S.empty (\_ -> ())
+compileProofE m e σ = case tyEnv' e of
+  Just γ -> compileProofE_ m e M.MTip γ σ S.empty (\_ -> ())
 
 
-{-@ compileProofE :: m:Nat
+{-@ compileProofE_ :: m:Nat
                   -> e:TypedLDSL p (Btwn 0 m)
                   -> γ:TyEnv' (Btwn 0 m)
                   -> γ':{TyEnv' (Btwn 0 m) | Just γ' = tyEnv'_ e γ}
@@ -115,114 +65,217 @@ compileProofE2 m e σ = case tyEnv' e of
 
                   -> ws:{S.Set (Btwn 0 m) | wfWire'_ ws e}
 
-                  -> ( j:{Btwn 0 m | S.member j (S.union ws (wiresE e))
+                  -> ( j:{Btwn 0 m | S.member j ws
                                   && M.lookup j γ' = Just TBool} ->
                         { boolean (M.lookup' j σ) } )
 
                   -> { coherentE m e σ <=>
                        satisfies (nGatesE e) m σ (compileE m e) } @-}
-compileProofE :: (Eq p, Fractional p) => Int -> LDSL p Int
+compileProofE_ :: (Eq p, Fractional p) => Int -> LDSL p Int
               -> TyEnv' Int -> TyEnv' Int
               -> WireValuation p -> S.Set Int -> (Int -> Proof) -> Proof
-compileProofE m e γ γ' σ ws π = case e of
+compileProofE_ m e γ γ' σ ws π = case e of
   LWIRE _ i -> trivial
   LVAR s τ i -> case τ of
     TF -> trivial
     TBool -> trivial
   LCONST x i -> trivial
   LBOOL  b i -> trivial
-  LDIV e1 e2 w i -> undefined
-                    -- case tyEnv'_ e1 γ of
-  --   Just γ1 -> case tyEnv'_ e2 γ1 of
-  --     Just γ2 -> compileProofE m eTotal γ e1 σ π ? compileProofE m eTotal γ e2 σ π
-  --              ? satisfiesDistr n1 n2 m σ (compileE m e1) (compileE m e2)
-  --              ? closedExpr m σ e1 ?? M.lookup' (outputWire e1) σ
-  --              ? closedExpr m σ e2 ?? M.lookup' (outputWire e2) σ
-  --   where n1 = nGatesE e1; n2 = nGatesE e2
+  LDIV e1 e2 w i -> case tyEnv'_ e1 γ of
+    Just γ1 -> case tyEnv'_ e2 γ1 of
+      Just γ2 -> case insertIfCompatible w TF γ2 of
+        Just γw -> ih1 ? ih2 ? satisfiesDistr n1 n2 m σ (compileE m e1) (compileE m e2)
+          where
+            n1 = nGatesE e1; n2 = nGatesE e2
 
-  LUN op e1 i -> undefined
-    --              case op of
-    -- ADDC _    -> proof
-    -- MULC _    -> proof
-    -- NOT       -> undefined
-    --           --    π (outputWire e1)
-    --           -- ?? liquidAssert (boolean (M.lookup' (outputWire e1) σ))
-    --           -- ?? proof
-    -- UnsafeNOT ->  undefined -- π (outputWire e1) ?? proof
-    -- where proof = compileProofE m eTotal γ e1 σ π
-    --             ? closedExpr m σ e1 ?? M.lookup' (outputWire e1) σ
+            {-@ ih1 :: { coherentE m e1 σ <=> satisfies n1 m σ (compileE m e1) } @-}
+            ih1 = compileProofE_ m e1 γ  γ1 σ ws π1 ? n1
+
+            {-@ ih2 :: { coherentE m e1 σ =>
+                        (coherentE m e2 σ <=> satisfies n2 m σ (compileE m e2) ) } @-}
+            ih2 = if coherentE m e1 σ
+                  then compileProofE_ m e2 γ1 γ2 σ (ws `S.union` wiresE e1) π2
+                  else trivial
+                  ? n2
+
+            {-@ π1 :: j:{Btwn 0 m | S.member j ws
+                                 && M.lookup j γ1 = Just TBool}
+                           -> { boolean (M.lookup' j σ) } @-}
+            π1 j = elementLemma j TBool γ1   -- j ∈ γ1
+                ?? lookupLemma j γ1          -- γ1[j] = TBool
+                ?? tyEnv'_incr e2 γ1 γ2 j    -- γ2[j] = γ1[j] since γ2 ≥ γ1
+                ?? insertICIncr w TF γ2 γw j -- γw[j] = γ2[j] since γw ≥ γ2
+                ?? insertICIncr i TF γw γ' j -- γ'[j] = γ2[j] since γ' ≥ γ2
+                ?? lookupLemma j γ'          -- M.lookup j γ' = Just γ'[j]
+                ?? π j
+
+            {-@ π2 :: j:{Btwn 0 m | S.member j (S.union ws (wiresE e1))
+                                 && M.lookup j γ2 = Just TBool}
+                           -> { coherentE m e1 σ => boolean (M.lookup' j σ) } @-}
+            π2 j = elementLemma j TBool γ2 -- j ∈ γ2
+                ?? lookupLemma j γ2        -- γ2[j] = TBool
+
+                ?? if S.member j ws -- j ∈ ws ∪ wires(e1); which one is it?
+                   -- if j ∈ ws
+                   then insertICIncr w TF γ2 γw j -- γ2[j] = γw[j] since γw ≥ γ2
+                     ?? insertICIncr i TF γw γ' j -- γw[j] = γ'[j] since γ' ≥ γw
+                     ?? lookupLemma j γ'          -- M.lookup j γ' = Just γ'[j]
+                     ?? π j                       -- σ[j] ∈ {0,1} since j ∈ ws
+
+                   -- if j ∈ wires(e1) (considering wires(e1) ⊆ keys(γ1))
+                   else tyEnv'_incr e2 γ1 γ2 j -- γ2[j] = γ1[j] since γ2 ≥ γ1
+                     ?? lookupLemma j γ1       -- M.lookup j γ1 = Just γ1[j]
+                     ?? booleanProof' m σ e1 γ γ1 j -- σ ⊢ e1 ⇒ σ[j] ∈ {0,1}
+
+
+  LUN op e1 i -> case tyEnv'_ e1 γ of
+    Just γ1 -> case op of
+      ADDC _ -> proof; MULC _ -> proof;
+      NOT -> proof
+          ?? booleanLemma1 m e1 σ γ γ1 ws π1
+      UnsafeNOT -> proof
+                ?? booleanLemma1 m e1 σ γ γ1 ws π1
+
+      where
+        {-@ ih1 :: { coherentE m e1 σ <=> satisfies n1 m σ (compileE m e1) } @-}
+        ih1 = compileProofE_ m e1 γ γ1 σ ws π1 ? n1
+
+        proof = ih1
+        n1 = nGatesE e1
+        τ = case inferType' e of Just t -> t
+
+        {-@ π1 :: j:{Btwn 0 m | S.member j ws
+                             && M.lookup j γ1 = Just TBool}
+                       -> { boolean (M.lookup' j σ) } @-}
+        π1 j = elementLemma j TBool γ1  -- j ∈ γ1
+            ?? lookupLemma j γ1         -- γ1[j] = TBool
+            ?? insertICIncr i τ γ1 γ' j -- γ'[j] = γ1[j] since γ' ≥ γ1
+            ?? lookupLemma j γ'         -- M.lookup j γ' = Just γ'[j]
+            ?? π j
 
   LBIN op e1 e2 i -> case tyEnv'_ e1 γ of
     Just γ1 -> case tyEnv'_ e2 γ1 of
       Just γ2 -> case op of
         ADD -> proof; SUB -> proof; MUL -> proof; LINCOMB _ _ -> proof;
-        AND ->        undefined;
-        UnsafeAND -> outputWireBool e1 γ  γ1
-                  -- ?? booleanProof' m σ e1 γ γ1 (outputWire e1) -- v1 ∈ {0,1}
-                  ?? wfWireLemma ws e1
-                  ?? π1 (outputWire e1) -- v1 ∈ {0,1}
+        AND -> proof
+            ?? booleanLemma1 m e1 σ γ γ1 ws π1
+            ?? booleanLemma2 m op e1 e2 i e σ γ γ1 γ2 γ' ws π1 π2
+        UnsafeAND -> proof
+                  ?? booleanLemma1 m e1 σ γ γ1 ws π1
+                  ?? booleanLemma2 m op e1 e2 i e σ γ γ1 γ2 γ' ws π1 π2
 
-                  ?? outputWireBool e2 γ1 γ2
-                  -- ?? booleanProof' m σ e2 γ1 γ2 (outputWire e2) -- v2 ∈ {0,1}
-                  ?? wfWireLemma (S.union ws (wiresE e1)) e2
-                  ?? π2 (outputWire e2) -- v2 ∈ {0,1}
+        OR  -> proof
+            ?? booleanLemma1 m e1 σ γ γ1 ws π1
+            ?? booleanLemma2 m op e1 e2 i e σ γ γ1 γ2 γ' ws π1 π2
+        UnsafeOR -> proof
+                 ?? booleanLemma1 m e1 σ γ γ1 ws π1
+                 ?? booleanLemma2 m op e1 e2 i e σ γ γ1 γ2 γ' ws π1 π2
 
-                  ?? proof;
+        XOR -> proof
+            ?? booleanLemma1 m e1 σ γ γ1 ws π1
+            ?? booleanLemma2 m op e1 e2 i e σ γ γ1 γ2 γ' ws π1 π2
+        UnsafeXOR -> proof
+                  ?? booleanLemma1 m e1 σ γ γ1 ws π1
+                  ?? booleanLemma2 m op e1 e2 i e σ γ γ1 γ2 γ' ws π1 π2
 
-        OR  ->        undefined -- proof;
-        UnsafeOR  ->  undefined -- proof;
+        where
+          {-@ ih1 :: { coherentE m e1 σ <=> satisfies n1 m σ (compileE m e1) } @-}
+          ih1 = compileProofE_ m e1 γ  γ1 σ ws π1
 
-        XOR ->        undefined -- proof;
-        UnsafeXOR ->  undefined -- proof;
+          {-@ ih2 :: { coherentE m e1 σ =>
+                      (coherentE m e2 σ <=> satisfies n2 m σ (compileE m e2) ) } @-}
+          ih2 = if coherentE m e1 σ
+                then compileProofE_ m e2 γ1 γ2 σ (ws `S.union` wiresE e1) π2
+                else trivial
 
-        where proof = compileProofE m e1 γ  γ1 σ ws                       π1
-                   ?? compileProofE m e2 γ1 γ2 σ (ws `S.union` wiresE e1) π2
-                   ?? satisfiesDistr n1 n2 m σ (compileE m e1) (compileE m e2)
-              n1 = nGatesE e1; n2 = nGatesE e2
+          proof = ih1 ? ih2 ? satisfiesDistr n1 n2 m σ (compileE m e1) (compileE m e2)
+          n1 = nGatesE e1; n2 = nGatesE e2
 
-              -- ws1 = ws `S.union` wiresE e1
+          τ = case inferType' e of Just t -> t
 
-              τ = case inferType' e of Just t -> t
+          {-@ π1 :: j:{Btwn 0 m | S.member j ws
+                               && M.lookup j γ1 = Just TBool}
+                         -> { boolean (M.lookup' j σ) } @-}
+          π1 j = elementLemma j TBool γ1  -- j ∈ γ1
+              ?? lookupLemma j γ1         -- γ1[j] = TBool
+              ?? tyEnv'_incr e2 γ1 γ2 j   -- γ2[j] = γ1[j] since γ2 ≥ γ1
+              ?? insertICIncr i τ γ2 γ' j -- γ'[j] = γ2[j] since γ' ≥ γ2
+              ?? lookupLemma j γ'         -- M.lookup j γ' = Just γ'[j]
+              ?? π j
 
-              --TODO: need to remove this
-              {-@ π1 :: j:{Btwn 0 m | S.member j (S.union ws (wiresE e1))
-                                   && M.lookup j γ1 = Just TBool}
-                             -> { boolean (M.lookup' j σ) } @-}
-              π1 j = elementLemma j TBool γ1
-                  ?? insertICIncr i τ γ2 γ' j -- γ2[j] = γ'[j] since γ' ≥ γ2
-                  ?? tyEnv'_incr e2 γ1 γ2 j   -- γ1[j] = γ2[j] since γ2 ≥ γ1
-                  ?? lookupLemma j γ1 ?? lookupLemma j γ'
-                  ?? π j
+          {-@ π2 :: j:{Btwn 0 m | S.member j (S.union ws (wiresE e1))
+                               && M.lookup j γ2 = Just TBool}
+                         -> { coherentE m e1 σ => boolean (M.lookup' j σ) } @-}
+          π2 j = elementLemma j TBool γ2 -- j ∈ γ2
+              ?? lookupLemma j γ2        -- γ2[j] = TBool
 
-              {-@ π2 :: j:{Btwn 0 m | S.member j (S.union (S.union ws (wiresE e1))
-                                                          (wiresE e2))
-                                   && M.lookup j γ2 = Just TBool}
-                             -> { boolean (M.lookup' j σ) } @-}
-              π2 j = elementLemma j TBool γ2
-                  ?? insertICIncr i τ γ2 γ' j -- γ2[j] = γ'[j] since γ' ≥ γ2
-                  ?? lookupLemma j γ2 ?? lookupLemma j γ'
-                  ?? π j
+              ?? if S.member j ws -- j ∈ ws ∪ wires(e1); which one is it?
+                 -- if j ∈ ws
+                 then insertICIncr i τ γ2 γ' j -- γ2[j] = γ'[j] since γ' ≥ γ2
+                   ?? lookupLemma j γ'         -- M.lookup j γ' = Just γ'[j]
+                   ?? π j                      -- σ[j] ∈ {0,1} since j ∈ ws
+
+                 -- if j ∈ wires(e1) (considering wires(e1) ⊆ keys(γ1))
+                 else tyEnv'_incr e2 γ1 γ2 j -- γ2[j] = γ1[j] since γ2 ≥ γ1
+                   ?? lookupLemma j γ1       -- M.lookup j γ1 = Just γ1[j]
+                   ?? booleanProof' m σ e1 γ γ1 j -- σ ⊢ e1 ⇒ σ[j] ∈ {0,1}
 
 
-  LBoolToF e1 -> compileProofE m e1 γ γ' σ ws π
-  LEQLC e1 k w i -> undefined
-    --                 case tyEnv'_ e1 γ of
-    -- Just γ1 -> case insertIfCompatible undefined
-    --                 compileProofE m e γ γ' e1 σ π -- (\j -> tyEnv'_incr e1 γ γ1 j ?? π j)
-    --               ? closedExpr m σ e1 ?? M.lookup' (outputWire e1) σ
+  LBoolToF e1 -> compileProofE_ m e1 γ γ' σ ws π
+  LEQLC e1 k w i -> case tyEnv'_ e1 γ of
+    Just γ1 -> case insertIfCompatible w TF γ1 of
+      Just γw -> compileProofE_ m e1 γ γ1 σ ws π1
+
+        where
+          {-@ π1 :: j:{Btwn 0 m | S.member j ws
+                               && M.lookup j γ1 = Just TBool}
+                         -> { boolean (M.lookup' j σ) } @-}
+          π1 j = elementLemma j TBool γ1      -- j ∈ γ1
+              ?? lookupLemma j γ1             -- γ1[j] = TBool
+              ?? insertICIncr w TF    γ1 γw j -- γw[j] = γ1[j] since γw ≥ γ1
+              ?? insertICIncr i TBool γw γ' j -- γ'[j] = γw[j] since γ' ≥ γw
+              ?? lookupLemma j γ'             -- M.lookup j γ' = Just γ'[j]
+              ?? π j
+
   LNIL _ -> trivial
-  LCONS e1 e2 -> undefined
-    --              case tyEnv'_ e1 γ of
-    -- Just γ1 -> compileProofE m e1 γ γ1 σ π1 ? compileProofE m e2 γ1 γ' σ π
-    --          ? satisfiesDistr n1 n2 m σ (compileE m e1) (compileE m e2)
-    --   where n1 = nGatesE e1; n2 = nGatesE e2
+  LCONS e1 e2 -> case tyEnv'_ e1 γ of
+    Just γ1 -> ih1 ?? ih2
+            ?? satisfiesDistr n1 n2 m σ (compileE m e1) (compileE m e2)
+      where
+        n1 = nGatesE e1; n2 = nGatesE e2
 
-    --         {-@ π1 :: j:{Btwn 0 m | M.lookup j γ1 = Just TBool}
-    --                        -> { boolean (M.lookup' j σ) } @-}
-    --         π1 j = elementLemma j TBool γ1
-    --             ?? tyEnv'_incr e2 γ1 γ' j
-    --             ?? lookupLemma j γ1 ?? lookupLemma j γ'
-    --             ?? π j
+        {-@ ih1 :: { coherentE m e1 σ <=> satisfies n1 m σ (compileE m e1) } @-}
+        ih1 = compileProofE_ m e1 γ  γ1 σ ws π1 ? n1
+
+        {-@ ih2 :: { coherentE m e1 σ =>
+                    (coherentE m e2 σ <=> satisfies n2 m σ (compileE m e2) ) } @-}
+        ih2 = if coherentE m e1 σ
+              then compileProofE_ m e2 γ1 γ' σ (ws `S.union` wiresE e1) π2
+              else trivial
+              ? n2
+
+        {-@ π1 :: j:{Btwn 0 m | S.member j ws
+                             && M.lookup j γ1 = Just TBool}
+                       -> { boolean (M.lookup' j σ) } @-}
+        π1 j = elementLemma j TBool γ1
+            ?? tyEnv'_incr e2 γ1 γ' j
+            ?? lookupLemma j γ1 ?? lookupLemma j γ'
+            ?? π j
+
+        {-@ π2 :: j:{Btwn 0 m | S.member j (S.union ws (wiresE e1))
+                             && M.lookup j γ' = Just TBool}
+                       -> { coherentE m e1 σ => boolean (M.lookup' j σ) } @-}
+        π2 j = elementLemma j TBool γ' -- j ∈ γ'
+            ?? lookupLemma j γ'        -- γ'[j] = TBool
+
+            ?? if S.member j ws -- j ∈ ws ∪ wires(e1); which one is it?
+               -- if j ∈ ws
+               then π j -- σ[j] ∈ {0,1} since j ∈ ws
+
+               -- if j ∈ wires(e1) (considering wires(e1) ⊆ keys(γ1))
+               else tyEnv'_incr e2 γ1 γ' j -- γ'[j] = γ1[j] since γ' ≥ γ1
+                 ?? lookupLemma j γ1       -- M.lookup j γ1 = Just γ1[j]
+                 ?? booleanProof' m σ e1 γ γ1 j -- σ ⊢ e1 ⇒ σ[j] ∈ {0,1}
 
 -- -- {-@ compileProofA :: m:Nat
 -- --                   -> a:LAss p (Btwn 0 m)
