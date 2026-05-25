c1b273b9c6d2614c4a2d627ae7ed861604c83146
Pablo Castellanos <p.cas.gar@gmail.com>
2026-05-25 11:32:41 +0200

    Prove second part of the Fundamental Theorem

--- src/FundamentalTheorem.hs
+++ src/FundamentalTheorem.hs
@@ -12,7 +12,7 @@ import WitnessGeneration
 
 import Utils
 
-import BooleanProof
+import BooleanProof hiding (foo, barOp)
 
 import qualified Data.Set as S
 
@@ -25,17 +25,11 @@ import qualified MapFunctions as M
 
 import MapLemmas
 import LabelingProof.LabelingLemmas
-import LabelingProof.RecursiveLemmas
 import LabelingProof.AgreeLemma
-import WitnessGenProof.WitnessGenLemmas
-import WitnessGenProof.SemanticsLemmas
 import WitnessGenProof.UniquenessLemmas
 import WitnessGenProof.Soundness
 import WitnessGenProof.Completeness
-
-import WitnessGenProof.UniquenessBase
-import WitnessGenProof.UniquenessOps
-import WitnessGenProof.Uniqueness2 --FIXME: these lemmas should go somewhere else
+import WitnessGenProof.Uniqueness
 
 import Language.Haskell.Liquid.ProofCombinators
 
@@ -71,4 +65,61 @@ fundamentalThmA m0 e ρ m e' λ v = ( σ ? sound
     sound = wf ?? wt ?? wgSoundE m ρ σ0 e' σ
     σ = wf ?? wgCompleteE m0 e ρ v λ0 σ0 (\_ -> ()) m e' λ
 
---TODO: missing part B of the theorem
+
+{-@ fundamentalThmB :: m0:Nat -> e:TypedDSL p
+                    -> ρ:NameValuation p
+
+                    -> m:{Nat | m0 <= m}
+                    -> e':{LDSL p (Btwn 0 m) | isJust (tyEnv' e')}
+                    -> λ:{LabelEnv p (Btwn 0 m) | label' e m0 M.MTip = (m, e', λ)}
+
+                    -> v:DSLValue p
+
+                    -> σ:{WireValuation p m | closedExpr m σ e'
+                                           && coherentE m e' σ
+                                           && evalWire m e' σ = v}
+                    -> Agree λ ρ σ
+
+                    -> { eval e ρ = Just v } @-}
+fundamentalThmB :: (Fractional p, Ord p) => Int -> DSL p
+                -> NameValuation p
+
+                -> Int -> LDSL p Int -> LabelEnv p Int
+                -> DSLValue p
+
+                -> WireValuation p -> (String -> Proof)
+
+                -> Proof
+fundamentalThmB m0 e ρ m e' λ v σ π =
+  wf ?? evalWireUnique m0 m e ρ λ0 m e' λ σ π v γ0 γ h_bool
+  where
+    λ0 = M.MTip
+
+    γ0 :: TyEnv' γ0
+    γ0 = M.MTip
+
+    wf = labelWF e m0 λ0 m e' λ
+    wt = labelTyped e m0 λ0 m e' λ
+
+    γ = wt ?? case tyEnv'_ e' γ0 of Just g -> g
+
+    {-@ h_bool :: j:{Btwn 0 m | S.member j (elemsSet λ)
+                             && M.lookup j γ = Just TBool}
+               -> { boolean (M.lookup' j σ) } @-}
+    h_bool j = labelWFWire' e m0 λ0 m e' λ
+            ?? liquidAssert (S.isSubsetOf (elemsSet λ) (S.union (elemsSet λ0) (wiresE e')))
+            ?? wt
+            ?? booleanProof' m σ e' γ0 γ j
+
+
+-- workarounds to fix "crash: unknown constant" --------------------------------
+
+{-@ reflect foo @-}
+foo :: UnOp Int -> Int
+foo (ADDC x) = x
+foo _        = 0
+
+{-@ reflect barOp @-}
+barOp :: BinOp Int -> Int
+barOp ADD = 0
+barOp _   = 1
